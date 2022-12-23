01e182132261e9d40667cad0f6b45efbcee34840
Admin <kqadb@mail.ru>
2022-12-24 03:21:17 +0500

    hi

--- src/homeWork.java
+++ src/homeWork.java
@@ -2,6 +2,18 @@ public class homeWork {
     public static void main (String [] args) {
 
 
-        
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+
+        String fullName1 = "Ivanov Ivan Ivanovich";
+        String upperFullName1 = fullName1.toUpperCase();
+        System.out.println(upperFullName1);
+
+        String fullName2 = "Иванов Семён Семёнович";
+        String replaceFullName2 = fullName2.replace('ё', 'е' );
+        System.out.println(replaceFullName2);
     }
 }
